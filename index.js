b4bb93c1ff6c6521a1211c003070528095e304b7
Fakhri Rossi <rossifakhri@gmail.com>
2024-06-19 11:14:36 +0700

    Added feature to access user cookie: using cookie-parser library

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 
 // define port
 const port = 3000;
@@ -7,7 +8,7 @@ const port = 3000;
 // define middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cookieParser());
 
 
 // Define routes
--- routes/movies.js
+++ routes/movies.js
@@ -2,12 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log('Visited');
-    res.send('Index Movies');
+    const { user = 'no-name', token = '' } = req.cookies;
+    console.log(user);
+    res.send(`Hello ${ user }, your token is ${token}`);
 });
 
 router.get('/create', (req, res) => {
     res.send('Create Movies');
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('user');
+    res.clearCookie('token');
+
+    if(!req.cookies.user && !req.cookies.token){
+        res.send('Your cookies has been cleared');
+
+    } else {
+        res.send('Your cookies has NOT been cleared');
+        
+    }
 });
 
 router.post('/', (req, res) => {
