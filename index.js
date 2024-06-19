fa884cdf57ef3e4ccda9c37184a1e421c3b905cd
Fakhri Rossi <rossifakhri@gmail.com>
2024-06-19 11:39:09 +0700

    Learning about signed cookie

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ const port = 3000;
 // define middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('secret-key'));
+
 
 
 // Define routes
+app.get('/signingcookie', (req, res) => {
+    res.cookie('paket', 'kaos', { signed: true });
+    res.send('Signed cookie');
+});
+
+app.get('/verifycookie', (req, res) => {
+    // const cookie = req.cookies;
+    const cookie = req.signedCookies;
+    res.send(cookie);
+})
+
 app.use('/theater', require('./routes/theater'));
 app.use('/movies', require('./routes/movies'));
 app.use('/admin', require('./routes/admin'));
